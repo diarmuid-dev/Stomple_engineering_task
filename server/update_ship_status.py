c5ac85b1475cb32b36636e8f7eb58b15222f3600
Diarmuid <dwyer.diarmuid@gmail.com>
2020-12-11 16:40:32 +1100

    Fix try/except error. Fix error in getting name from spaceship

--- server/update_ship_status.py
+++ server/update_ship_status.py
@@ -9,27 +9,27 @@ def update_ship_status(ship_id, status):
 
     try:
         ship_id = int(ship_id)
-    except exception as e:
+    except Exception as e:
         return(f"Invalid year {ship_id}")
 
     try:
         db = psycopg2.connect("dbname=stomple")
         curr = db.cursor()
 
+        curr.execute(f"select name from spaceships where id = {ship_id};")
+
+        name = curr.fetchall()
+
         # Insert new location into db
         curr.execute(f"update spaceships set status = '{status}' where id = {ship_id};")
 
         db.commit()
 
-        curr.execute(f"select name from spaceships where id = {ship_id};")
-
-        name = curr.fetchall()
-
         return (f"Successfully updated {name[0][0]} to {status}")
 
     except psycopg2.Error as err:
         print ("ERROR" + str(err))
-        return err
+        return str(err)
     finally:
         if db:
             db.close()
