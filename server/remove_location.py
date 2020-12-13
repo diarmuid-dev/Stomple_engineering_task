caa816ddd09c30bfca2f60a766fd5b40f8b95eb1
Diarmuid <dwyer.diarmuid@gmail.com>
2020-12-13 14:38:53 +1100

    Fix bug with checking the number of ships at a location. Add check for location existence.

--- server/remove_location.py
+++ server/remove_location.py
@@ -1,4 +1,5 @@
 import psycopg2
+from utils import locationExists
 
 def remove_location(location_id):
     db = None
@@ -13,6 +14,9 @@ def remove_location(location_id):
         db = psycopg2.connect("dbname=stomple")
         curr = db.cursor()
 
+        if (not locationExists(curr, location_id)):
+            return "Error: this location does not exist"
+
         # Check to see if there are spaceships at this location. If there are,
         # We cannot delete the location
         curr.execute(f"""select s.name from spaceships s
@@ -20,20 +24,20 @@ def remove_location(location_id):
                          where l.id = {location_id};""")
         name = curr.fetchall()
 
-        if (len(name) < 1):
+        if (len(name) > 0):
             return "Error deleting location: There are still spaceShips at this location"
 
         curr.execute(f"""select city_name
                          from locations
                          where id = {location_id};""")
-        name = curr.fetchall()
+        name = curr.fetchone()
 
         # remove spaceship from db
         curr.execute(f"delete from locations where id = {location_id};")
 
         db.commit()
 
-        return (f"""Successfully removed the location {name[0][0]}""")
+        return (f"""Successfully removed the location {name[0]}""")
 
     except psycopg2.Error as err:
         return f"Error deleting location;    " + str(err)
